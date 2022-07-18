953fdbd50ea57e584626760ff029751e2bc1c6bb
AnHai17 <anbh.1704@gmail.com>
2022-07-18 15:35:13 +0700

    18/7

---
+++ app.js
@@ -0,0 +1,26 @@
+const shortURLForm = document.querySelector(".shortURLForm");
+const inputURL = document.getElementById("inputURL");
+const url = "http://localhost:8080/shortURL/request";
+const resultURL = document.getElementById("resultURL");
+
+const renderData = (shortURL) => {
+  console.log(shortURL.short62);
+  let finalResult = shortURL.short62;
+  resultURL.href = finalResult;
+  resultURL.innerHTML = finalResult;
+};
+shortURLForm.addEventListener("submit", (e) => {
+  console.log(inputURL.value);
+  e.preventDefault();
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      longUrl: inputURL.value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => renderData(data));
+});
