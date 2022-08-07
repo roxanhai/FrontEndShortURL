e149fec6ef373c3a0b3ece77c914b7009a99090a
AnHai17 <anbh.1704@gmail.com>
2022-08-08 02:34:19 +0700

    8/8

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const shortURLForm = document.querySelector(".shortURLForm");
-const inputURL = document.getElementById("inputURL");
+const shortURLForm = document.querySelector(".form");
+const inputURL = document.getElementById("create-short-url");
 const url = "http://localhost:8080/shortURL/request";
-const resultURL = document.getElementById("resultURL");
+const resultURL = document.getElementById("short-url");
 
 const renderData = (shortURL) => {
   console.log(shortURL.short62);
@@ -24,3 +24,30 @@ shortURLForm.addEventListener("submit", (e) => {
     .then((res) => res.json())
     .then((data) => renderData(data));
 });
+
+// const shortURLForm = document.querySelector(".shortURLForm");
+// const inputURL = document.getElementById("inputURL");
+// const url = "http://localhost:8080/shortURL/request";
+// const resultURL = document.getElementById("resultURL");
+
+// const renderData = (shortURL) => {
+//   console.log(shortURL.short62);
+//   let finalResult = shortURL.short62;
+//   resultURL.href = finalResult;
+//   resultURL.innerHTML = finalResult;
+// };
+// shortURLForm.addEventListener("submit", (e) => {
+//   console.log(inputURL.value);
+//   e.preventDefault();
+//   fetch(url, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       longUrl: inputURL.value,
+//     }),
+//   })
+//     .then((res) => res.json())
+//     .then((data) => renderData(data));
+// });
